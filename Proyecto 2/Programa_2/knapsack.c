3ec46b4351c3627ce0079a244c485a324ef2c56c
JoqTheJJ <josuesogo@gmail.com>
2025-09-07 16:32:20 -0600

    fixed bugs

--- Proyecto 2/Programa_2/knapsack.c
+++ Proyecto 2/Programa_2/knapsack.c
@@ -13,8 +13,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include <gtk/gtk.h>
-#include <cairo.h>
 #include <ctype.h>
 
 
@@ -33,13 +31,32 @@ int ipow(int base, int exp) {
 
 /* ################################## KNAPSACK ################################## */
 
-struct Cell {
+typedef struct {
     int max;
     int ganadores;
+} Cell;
+
+
+void printCellMatrix(Cell** matrix, int rows, int cols){
+    for (int r = 0; r < rows; ++r){
+        if (r < 10)
+            printf("0");
+
+        printf("%d [ ", r);
+        for (int c = 0; c < cols; ++c){
+
+            int res = matrix[r][c].max;
+            if (res < 10)
+                printf(" ");
+
+            printf("%d{%d}  ", res, matrix[r][c].ganadores);
+        }
+        printf("]\n");
+    }
 }
 
-
-Cell** knapsack(int n, int capacity, int* profits, int* costs, int* quantity){
+Cell** knapsack(int n, int maxCapacity, int* profits, int* costs, int* quantity){
+    int capacity = maxCapacity + 1;
 
     Cell** res = malloc(sizeof(Cell*) * (capacity));
     for (int i = 0; i < capacity; ++i){
@@ -52,9 +69,14 @@ Cell** knapsack(int n, int capacity, int* profits, int* costs, int* quantity){
 
             int max = -1;
             int newCapacity;
-            for (int q = 0; q < quantity[obj]; ++q){
-                newCapacity = capacity - q*costs[obj];
-                current = q*profits[obj] + res[newCapacity][obj]; //es obj-1 implicito
+            int current;
+            for (int q = 0; q <= quantity[obj]; ++q){
+
+                newCapacity = c - q*costs[obj];
+                if (newCapacity < 0){ //Exceeds capacity
+                    break;
+                }
+                current = q*profits[obj] + res[newCapacity][obj].max; //es obj-1 implicito
 
                 if (current > max){
                     max = current;
@@ -83,16 +105,46 @@ Cell** knapsack(int n, int capacity, int* profits, int* costs, int* quantity){
 
 
 
-/* ################################## TEX ################################## */
-
-
-
 /* ################################## MAIN ################################## */
 
-int main(int argc, char *argv[]) {
+int main() {
 
-        
-    calculateC(); //Receives all of the above :P
+    //Inputs
+    int n = 7;
+    int capacity = 15;
+    
+    int* profits = malloc(sizeof(int) * n);
+    profits[0] = 7;
+    profits[1] = 9;
+    profits[2] = 5;
+    profits[3] = 12;
+    profits[4] = 14;
+    profits[5] = 6;
+    profits[6] = 12;
+
+    int* costs = malloc(sizeof(int) * n);
+    costs[0] = 3;
+    costs[1] = 4;
+    costs[2] = 2;
+    costs[3] = 6;
+    costs[4] = 7;
+    costs[5] = 3;
+    costs[6] = 5;
+
+    int* quantity = malloc(sizeof(int) * n);
+    quantity[0] = 1;
+    quantity[1] = 1;
+    quantity[2] = 1;
+    quantity[3] = 1;
+    quantity[4] = 1;
+    quantity[5] = 1;
+    quantity[6] = 1;
+
+
+    Cell** answer;
+    answer = knapsack(n, capacity, profits, costs, quantity);
+
+    printCellMatrix(answer, capacity+1, n+1);
 
 
     return 0;
